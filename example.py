904fda66f6ac18f66e9fe730892cb06149890e8c
BenitzCoding <benitzcoding@yahoo.com>
2022-12-19 21:46:31 +0800

    Inital Commit.

---
+++ example.py
@@ -0,0 +1,50 @@
+from api_gateway import Discord, Interaction, Choice, AutoComplete
+from typing import Optional
+
+gateway = Discord.GatewayClient(
+    api_version = 11,
+    secret_key = "SECRET",
+    public_key = "PUBLIC",
+    token =  "TOKEN"
+)
+
+@gateway.command("test")
+@gateway.choices(
+    "choice",
+    [
+        Choice("Name", "value"),
+        Choice("Name2", "value2")
+    ]
+)
+@gateway.description("This is a test command.")
+async def test_command(
+    interaction: Interaction,
+    choice: Optional[Choice],
+    autocomplete: AutoComplete("name")
+):
+    await interaction.reply("Hello World!", ephemeral = True)
+
+@gateway.autocomplete("name")
+async def test_autocomplete(interaction: Interaction):
+    return [
+        Choice("Name", "value"),
+        Choice("Name2", "value2")
+    ]
+
+@gateway.button("custom_id")
+async def test_button(interaction: Interaction):
+    await interaction.respond(
+        {
+            "type": 4,
+            "data": {
+                "content": "Hello World!",
+                "flags": 64
+            }
+        }
+    )
+
+@gateway.event("interaction_receive")
+async def interaction_receive(interaction: Interaction):
+    print(interaction)
+
+gateway.run()
---
+++ src/discord.py
@@ -0,0 +1,151 @@
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
+
+from fastapi import FastAPI, Request
+
+class Interaction:
+    def __init__(self, interaction_payload: dict):
+        for key, value in interaction_payload.items():
+            setattr(self, key, value)
+
+    def respond(self, response: dict):
+        return response
+
+    async def reply(self, content: str, embeds: list = None, ephemeral: bool = False, flags = 0):
+        return {
+            "type": 4,
+            "data": {
+                "content": content,
+                "embeds": embeds,
+                "flags": 64 if ephemeral and not flags else flags
+            }
+        }
+
+class Discord:
+
+    class GatewayClient:
+        def __init__(self, api_version: int, secret_key: str, public_key: str, token: str, port: int = 80):
+            self.api_version = api_version
+            self.secret_key = secret_key
+            self.public_key = public_key
+            self.token = token
+            self.port = port
+
+            self.autocomplete = {}
+            self.commands = {}
+            self.buttons = {}
+            self.events = {}
+
+        def command(self, name: str):
+            def decorator(func):
+                self.commands.update(
+                    {
+                        name: {
+                            "function": func,
+                            "description": self.commands.get("description"),
+                            "options": self.commands.get("options")
+                        }
+                    }
+                )
+                self.commands[name] = func
+                return func
+            return decorator
+
+        def button(self, custom_id: str):
+            def decorator(func):
+                self.buttons[custom_id] = func
+                return func
+            return decorator
+
+        def description(self, description: str):
+            def decorator(func):
+                self.commands[func.__name__].description = description
+                return func
+            return decorator
+
+        def button(self, custom_id: str):
+            def decorator(func):
+                self.commands[custom_id] = func
+                return func
+            return decorator
+
+        def event(self, event: str):
+            def decorator(func):
+                self.events[event] = func
+                return func
+            return decorator
+
+        def run(self):
+            app = FastAPI()
+
+            @app.get("/")
+            async def index(request: Request):
+                return "This is a Discord Interaction API.", 200
+
+            @app.post("/interaction")
+            async def interaction(request: Request):
+                # Verify the request.
+                verify_key = VerifyKey(bytes.fromhex(self.public_key))
+                signature = request.headers.get("X-Signature-Ed25519")
+                timestamp = request.headers.get("X-Signature-Timestamp")
+                body = (await request.body()).decode("utf-8")
+
+                try:
+                    verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature))
+                except BadSignatureError:
+                    return 'invalid request signature', 401
+
+                # Process the request.
+                interaction_payload = await request.json()
+
+                if interaction_payload["type"] == 1:
+                    return {
+                        "type": 1
+                    }
+
+                elif interaction_payload["type"] == 2:
+                    if interaction_payload["data"]["name"] in self.commands:
+                        await self.events["interaction_receive"](interaction_payload)
+                        return await self.commands[interaction_payload["data"]["name"]](interaction_payload)
+
+                    return {
+                        "type": 4,
+                        "data": {
+                            "content": "This command is not registered with Interaction Gateway API.",
+                            "flags": 64
+                        }
+                    }
+
+                elif interaction_payload["type"] == 3:
+                    if interaction_payload["data"]["custom_id"] in self.buttons:
+                        await self.events["interaction_receive"](interaction_payload)
+                        return await self.buttons[interaction_payload["data"]["custom_id"]](interaction_payload)
+
+                    return {
+                        "type": 4,
+                        "data": {
+                            "content": "This button is not registered with Interaction Gateway API.",
+                            "flags": 64
+                        }
+                    }
+
+                elif interaction_payload["type"] == 4:
+                    if interaction_payload["data"]["custom_id"] in self.buttons:
+                        await self.events["interaction_receive"](interaction_payload)
+                        return await self.buttons[interaction_payload["data"]["custom_id"]](interaction_payload)
+
+                    return {
+                        "type": 4,
+                        "data": {
+                            "content": "This button is not registered with Interaction Gateway API.",
+                            "flags": 64
+                        }
+                    }
+
+                elif interaction_payload["type"] == 5:
+                    if interaction_payload["data"][""]
+
+            app.run(
+                host = "127.0.0.1",
+                port = self.port
+            )
