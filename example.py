aba07100f611146d3399de88e8b6e37c7e287e61
BenitzCoding <benitzcoding@yahoo.com>
2022-12-23 23:12:57 +0800

    Updated Docs.

--- example.py
+++ example.py
@@ -8,26 +8,34 @@ gateway = GatewayClient(
     token =  "TOKEN"
 )
 
-@gateway.register(
-    CommandInteraction(
-        name = "test",
-        description = "Test command",
-        options = [
-            Option(
-                type = OptionType.STRING,
-                name = "name",
-                description = "Name",
-                required = True
-            ),
-            Option(
-                type = OptionType.STRING,
-                name = "name2",
-                description = "Name2",
-                required = False
-            )
-        ]
-    )
+button_interaction = ButtonInteraction(
+    custom_id = "test"
 )
+
+autocomplete_interaction = AutoComplete(
+    ...
+)
+
+test = CommandInteraction(
+    name = "test",
+    description = "Test command",
+    options = [
+        Option(
+            type = OptionType.STRING,
+            name = "name",
+            description = "Name",
+            required = True
+        ),
+        Option(
+            type = OptionType.STRING,
+            name = "name2",
+            description = "Name2",
+            required = False
+        )
+    ]
+)
+
+@gateway.on(test)
 async def test_command(
     interaction: Interaction,
     name: Optional[Choice],
@@ -35,14 +43,14 @@ async def test_command(
 ):
     interaction.reply("Hello World!", ephemeral = True)
 
-@gateway.autocomplete("name")
+@gateway.on(autocomplete_interaction)
 async def test_autocomplete(interaction: Interaction):
     return [
         Choice("Name", "value"),
         Choice("Name2", "value2")
     ]
 
-@gateway.button("custom_id")
+@gateway.on(button_interaction)
 async def test_button(interaction: Interaction):
     await interaction.respond(
         {
@@ -54,7 +62,7 @@ async def test_button(interaction: Interaction):
         }
     )
 
-@gateway.event("interaction_receive")
+@gateway.on("interaction_receive")
 async def interaction_receive(interaction: Interaction):
     print(interaction)
 
