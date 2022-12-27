5dc5f979e5d4b607044202a65189c37b3f7cecde
BenitzCoding <benitzcoding@yahoo.com>
2022-12-27 19:11:45 +0800

    Added `Button` Class.

---
+++ gatepoint/components/button.py
@@ -0,0 +1,17 @@
+
+
+class Button:
+	def __init__(
+		self,
+		label: str,
+		custom_id: str,
+		url: str = None,
+		style: int = None
+	):
+		if len(custom_id) > 100 or len(custom_id) == 0:
+			raise ValueError("custom_id: Must be more than 0 characters and less than 100 characters.")
+
+		self.label = label
+		self.custom_id = custom_id
+		self.style = 5 if url else style or 1
+		self.url = url
