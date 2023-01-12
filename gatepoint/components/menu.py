5f401b46c09adb22632fb1fb2aed60440cbd85be
BenitzCoding <benitzcoding@yahoo.com>
2023-01-12 23:35:38 +0800

    Fixed Component types.

--- gatepoint/components/menu.py
+++ gatepoint/components/menu.py
@@ -67,22 +67,22 @@ class UserSelect:
         self.disabled = disabled
 
     def to_dict(self):
-        return {
-            "type": 3,
+        dict_ = {
+            "type": 5,
             "custom_id": self.custom_id,
-            "placeholder": self.placeholder,
-            "min_values": self.min_values,
-            "max_values": self.max_values,
-            "disabled": self.disabled
         }
 
 class MentionableSelect(SelectMenu):
     def __init__(self, custom_id: str, placeholder: str = None, min_values: int = 1, max_values: int = 1, disabled: bool = False):
-        super().__init__(custom_id, [], placeholder, min_values, max_values, disabled)
+        self.custom_id = custom_id
+        self.placeholder = placeholder
+        self.min_values = min_values
+        self.max_values = max_values
+        self.disabled = disabled
 
     def to_dict(self):
         return {
-            "type": 3,
+            "type": 6,
             "custom_id": self.custom_id,
             "placeholder": self.placeholder,
             "min_values": self.min_values,
@@ -92,11 +92,15 @@ class MentionableSelect(SelectMenu):
 
 class MentionableSelect:
     def __init__(self, custom_id: str, placeholder: str = None, min_values: int = 1, max_values: int = 1, disabled: bool = False):
-        super().__init__(custom_id, [], placeholder, min_values, max_values, disabled)
+        self.custom_id = custom_id
+        self.placeholder = placeholder
+        self.min_values = min_values
+        self.max_values = max_values
+        self.disabled = disabled
 
     def to_dict(self):
         return {
-            "type": 3,
+            "type": 7,
             "custom_id": self.custom_id,
             "placeholder": self.placeholder,
             "min_values": self.min_values,
@@ -114,9 +118,8 @@ class ChannelSelect:
 
     def to_dict(self):
         return {
-            "type": 3,
+            "type": 8,
             "custom_id": self.custom_id,
-            "options": [option.to_dict() for option in self.options],
             "placeholder": self.placeholder,
             "min_values": self.min_values,
             "max_values": self.max_values,
