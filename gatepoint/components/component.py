b31d3085ef41a9ca21d2edb732116216debce815
BenitzCoding <benitzcoding@yahoo.com>
2023-01-12 23:31:28 +0800

    Removed Component object.

--- gatepoint/components/component.py
+++ gatepoint/components/component.py
@@ -9,12 +9,3 @@ class ActionRow:
         if len(self.components) > 5:
             raise ValueError("You can only have 5 components per row")
 
-class Component(list):
-    def __init__(self, components: Union[ActionRow, List[ActionRow]]):
-        if isinstance(components, ActionRow):
-            self.components = [components]
-        else:
-            self.components = components
-
-    def __iter__(self):
-        return iter(self.components)
