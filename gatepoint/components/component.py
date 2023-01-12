efdbfe97e16159a001733b927e4527e57497e052
BenitzCoding <benitzcoding@yahoo.com>
2023-01-12 23:30:19 +0800

    Fixed minor bug.

--- gatepoint/components/component.py
+++ gatepoint/components/component.py
@@ -6,7 +6,7 @@ from .menu import SelectMenu, UserSelect, RoleSelect, MentionableSelect, Channel
 class ActionRow:
     def __init__(self, *components: Union[Button, SelectMenu, UserSelect, RoleSelect, MentionableSelect, ChannelSelect]):
         self.components = components
-        if self.components > 5:
+        if len(self.components) > 5:
             raise ValueError("You can only have 5 components per row")
 
 class Component(list):
