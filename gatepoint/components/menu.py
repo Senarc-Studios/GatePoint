f89f47d909ca23ba71d4965f925bdc34e6232388
BenitzCoding <benitzcoding@yahoo.com>
2023-01-12 23:36:17 +0800

    Fixed dictionary conversions.

--- gatepoint/components/menu.py
+++ gatepoint/components/menu.py
@@ -20,28 +20,20 @@ class SelectMenu:
         self.disabled = disabled
 
     def to_dict(self):
-        return {
+        dict_ = {
             "type": 3,
             "custom_id": self.custom_id,
-            "options": [option.to_dict() for option in self.options],
-            "placeholder": self.placeholder,
-            "min_values": self.min_values,
-            "max_values": self.max_values,
-            "disabled": self.disabled
+            "options": [
+                option.to_dict()
+                for option in self.options
+            ],
         }
 
         if self.placeholder:
             dict_["placeholder"] = self.placeholder
 
-    def to_dict(self):
-        return {
-            "type": 3,
-            "custom_id": self.custom_id,
-            "placeholder": self.placeholder,
-            "min_values": self.min_values,
-            "max_values": self.max_values,
-            "disabled": self.disabled
-        }
+        if self.min_values:
+            dict_["min_values"] = self.min_values
 
         if self.max_values:
             dict_["max_values"] = self.max_values
@@ -72,7 +64,21 @@ class UserSelect:
             "custom_id": self.custom_id,
         }
 
-class MentionableSelect(SelectMenu):
+        if self.placeholder:
+            dict_["placeholder"] = self.placeholder
+
+        if self.min_values:
+            dict_["min_values"] = self.min_values
+
+        if self.max_values:
+            dict_["max_values"] = self.max_values
+
+        if self.disabled:
+            dict_["disabled"] = self.disabled
+
+        return dict_
+
+class RoleSelect:
     def __init__(self, custom_id: str, placeholder: str = None, min_values: int = 1, max_values: int = 1, disabled: bool = False):
         self.custom_id = custom_id
         self.placeholder = placeholder
