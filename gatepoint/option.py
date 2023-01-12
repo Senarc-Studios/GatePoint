0ad1d03108ce0e625447ccaf02253dcdd411c5ff
BenitzCoding <benitzcoding@yahoo.com>
2023-01-12 23:41:30 +0800

    Added `MenuOption` for `SelectMenu` objects.

--- gatepoint/option.py
+++ gatepoint/option.py
@@ -33,3 +33,36 @@ class CommandOption:
             "name": self.name,
             "value": self.value
         }
+
+class MenuOption:
+    def __init__(
+        self,
+        label: str,
+        value: str,
+        description: Optional[str] = None,
+        emoji: Optional[dict] = None,
+        default: Optional[bool] = False
+    ):
+        self.type = type
+        self.label = label
+        self.value = value
+        self.description = description
+        self.emoji = emoji
+        self.default = default
+
+    def to_dict(self):
+        dict_ = {
+            "label": self.label,
+            "value": self.value
+        }
+
+        if self.description:
+            dict_["description"] = self.description
+
+        if self.emoji:
+            dict_["emoji"] = self.emoji
+
+        if self.default:
+            dict_["default"] = self.default
+
+        return dict_
